3f65c251ba4ddfee56e045bd0b926517470edf37
Taras Rashnik <Taras_Rashnik@epam.com>
2017-10-12 11:57:31 +0200

    add dummy layout for the main page

---
+++ src/main_page/layout.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class Layout extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Main Layout</h1>
+        <section>Main</section>
+        <section>Decks pane</section>
+        <section>Cards pane</section>
+      </div>
+    );
+  }
+}
