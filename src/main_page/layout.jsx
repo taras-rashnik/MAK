95299d269b290d0e9825c0d4b27b90a37af9d01a
Taras Taras <TarasTaras@MacBook-Taras.local>
2017-10-14 15:08:56 +0200

    add SplitterLayout

--- src/main_page/layout.jsx
+++ src/main_page/layout.jsx
@@ -1,14 +1,16 @@
 import React, {Component} from "react";
+import SplitterLayout from 'react-splitter-layout';
 
 export default class Layout extends Component {
   render(){
     return (
-      <div>
-        <h1>Main Layout</h1>
-        <section>Main</section>
-        <section>Decks pane</section>
-        <section>Cards pane</section>
-      </div>
+      <SplitterLayout vertical={true} primaryIndex={0} primaryMinSize={400} secondaryMinSize={50} secondaryInitialSize={200}>
+        <SplitterLayout primaryIndex={0} primaryMinSize={400} secondaryMinSize={50} secondaryInitialSize={200}>
+          <div>Main pane</div>
+          <div>Right pane</div>
+        </SplitterLayout>
+        <div>Bottom pane</div>
+      </SplitterLayout>
     );
   }
 }
